e3a7023b78c3fdc65a5017c2f8102ee90dad9c4d
Tejas Shah <shah.te@northeastern.edu>
2020-06-29 14:55:19 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
+import { InstantSearch } from 'react-instantsearch-dom';
 import Hits from './components/Hits';
 import CustomSearchBox from './components/CustomSearchBox';
+import './App.css';
 
 const searchClient = algoliasearch('0RDP6I1JSB', '30cfaf989bcf8a1885f112de273eb0ed');
 
 function App() {
   return (
     <div className="App">
+      <div className="title">Search GIFs</div>
       <InstantSearch searchClient={searchClient} indexName="giphy">
         <CustomSearchBox autoFocus searchAsYouType={false} />
         <Hits />
